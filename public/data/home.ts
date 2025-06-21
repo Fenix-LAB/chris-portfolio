441c6ef1e51f80b61b62ce363147ce0f89d38675
Fenix-LAB <fenixcr.123@gmail.com>
2025-06-21 13:32:59 -0600

    Update contact information and social links in homeData

--- public/data/home.ts
+++ public/data/home.ts
@@ -14,52 +14,52 @@ export const homeData = {
   contactInfo: [
     {
       Icon: IoMdMail,
-      Label: "kareem.e.hamouda@gmail.com",
+      Label: "fenixcr.123@gmail.com",
       Link: "/contact", // This refers to the Contact page in the website, you can change it to whatever you like
     },
     {
       Icon: FaLocationDot,
-      Label: "Cairo, Egypt",
-      Link: "https://www.google.com/maps/place/Cairo,+Cairo+Governorate/@30.0594885,31.2584644,13z", // This is just a simple location of my city
+      Label: "CDMX, Mexico",
+      Link: "https://www.google.com/maps/place/Ciudad+de+México,+CDMX/@19.3904366,-99.4732714,10z/data=!3m1!4b1!4m6!3m5!1s0x85ce0026db097507:0x54061076265ee841!8m2!3d19.4326077!4d-99.133208!16zL20vMDRzcWo?entry=ttu&g_ep=EgoyMDI1MDYxNy4wIKXMDSoASAFQAw%3D%3D", // This is just a simple location of my city
     },
     {
       Icon: FaPhoneAlt,
-      Label: "+20 115 111 9085",
-      Link: "tel:+201151119085",
+      Label: "+52 55 4183 2110",
+      Link: "tel:+525541832116",
     },
   ],
   education: [
     {
       Icon: FaUniversity,
-      Label: "Ain Shams University",
-      Link: "https://www.asu.edu.eg/",
+      Label: "Benemerita Universidad Autonoma de Puebla",
+      Link: "https://www.buap.mx",
     },
     {
       Icon: FaGraduationCap,
-      Label: "Bachelor's Degree in Computer Science",
-      Link: "https://cis.asu.edu.eg/",
+      Label: "Bachelor's Degree in Mechatronic Engineer",
+      Link: "https://serviciosdes.buap.mx/oferta-educativa/programa/LIM-2025",
     },
   ],
   social: [
     {
       Icon: FaGithub,
       Label: "GitHub",
-      Link: "https://github.com/KareemEhab",
+      Link: "https://github.com/Fenix-LAB",
     },
     {
       Icon: FaLinkedin,
       Label: "LinkedIn",
-      Link: "https://www.linkedin.com/in/kareem-hamouda/",
+      Link: "https://www.linkedin.com/in/christian-rodríguez-hernández-448451247/",
     },
     {
       Icon: FaFacebook,
       Label: "Facebook",
-      Link: "https://www.facebook.com/kareem.ehab.37",
+      Link: "https://www.facebook.com/",
     },
     {
       Icon: FaInstagram,
       Label: "Instagram",
-      Link: "https://www.instagram.com/kareem.ehabz/",
+      Link: "https://www.instagram.com/",
     },
   ],
 };
